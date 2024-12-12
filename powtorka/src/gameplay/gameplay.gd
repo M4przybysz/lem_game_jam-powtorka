bf09ac77a8be04a04dd8c1d7f901825bc97b6a38
Maciej <maprz2017@gmail.com>
2024-12-12 22:27:24 +0100

    Added loop system and ways of showing objects

---
+++ powtorka/src/gameplay/gameplay.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var player: CharacterBody2D = $Player
+
+var loop_count: int = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("action2"):
+		loop_count += 1
+		player.reset_position()
