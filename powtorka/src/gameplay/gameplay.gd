71a3bcd72a4d8d48265850e2cebddef36159d336
Maciej <maprz2017@gmail.com>
2024-12-13 10:32:15 +0100

    Fixed holding items and added teleports

--- powtorka/src/gameplay/gameplay.gd
+++ powtorka/src/gameplay/gameplay.gd
@@ -20,6 +20,9 @@ func _input(event: InputEvent) -> void:
 func reset_loop():
 		loop_count += 1
 		player.reset_position()
+		$Kubeczek.reset_position()
+		$Krzeslo4.reset_position()
+		$PlakietkaIDEwy.reset_position()
 
 func teleport_player(x: int, y: int):
 	player.global_position.x = x
